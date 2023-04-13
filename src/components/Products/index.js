055a4570579a4d31b9f1fc79542137e6f811ad3c
vijaypotnuru <vijaypotnuru123@gmail.com>
2023-04-13 09:45:09 +0000

    some changes in code

---
+++ src/components/Products/index.js
@@ -0,0 +1,30 @@
+import Cookies from 'js-cookie'
+
+import {Redirect} from 'react-router-dom'
+
+import Header from '../Header'
+
+import './index.css'
+
+const Products = () => {
+  const jwtToken = Cookies.get('jwt_token')
+
+  if (jwtToken === undefined) {
+    return <Redirect to="/login" />
+  }
+
+  return (
+    <>
+      <Header />
+      <div className="products-container">
+        <img
+          className="products-img"
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+          alt="products"
+        />
+      </div>
+    </>
+  )
+}
+
+export default Products
